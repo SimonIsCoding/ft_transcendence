f760d87163b0eeea2b418f94f51e69c74899feab
SimArch21 <simon.archambault@hotmail.fr>
2025-06-06 14:22:48 +0200

    black square on webpage

---
+++ srcs/docker/website/dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+window.addEventListener("load", () => {
+    const canvas = document.getElementById("pong_table");
+    const ctx = canvas.getContext("2d");
+    if (ctx) {
+        ctx.fillStyle = "black";
+        ctx.fillRect(50, 50, 100, 100);
+    }
+});
--- srcs/docker/website/server.js
+++ srcs/docker/website/server.js
@@ -5,6 +5,7 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 app.use('/website', express.static('public'));
+app.use('/dist', express.static('dist'));
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
