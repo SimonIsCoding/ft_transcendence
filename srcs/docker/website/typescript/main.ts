c5a345eea6ce0181ff64eec29654443a8bfe0b2b
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-06-06 18:01:27 +0200

    left paddle moving

--- srcs/docker/website/typescript/main.ts
+++ srcs/docker/website/typescript/main.ts
@@ -1,9 +1,29 @@
-window.addEventListener("load", () => {
-	const canvas = document.getElementById("pong_table") as HTMLCanvasElement;
-	const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("pong_table") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d")!;
 
-	if (ctx) {
-		ctx.fillStyle = "purple";
-		ctx.fillRect(50, 50, 100, 100);
-	}
-});
+const leftPaddle = {
+	x: 0,
+	y: 0,
+	width: 10,
+	height: 125
+};
+
+const interval = setInterval(() => {
+  if (leftPaddle.y > 475)
+	  return;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);// Efface le canvas
+
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);// Redessine le fond
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);// Dessine la raquette
+  leftPaddle.y += 25;
+  console.log("valeur de leftPaddle.y = ", leftPaddle.y);
+}, 10); // toutes les 10 ms
+
+
+// // if (left_paddle) {
+// // 		left_paddle.fillStyle = "white";
+// // 	left_paddle.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height); //fillRect to draw
+// // }
