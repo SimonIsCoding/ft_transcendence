56983ef6e10ce62bfd46d89b03e7bd02e94deb16
SimArch21 <simon.archambault@hotmail.fr>
2025-06-07 18:44:17 +0200

    scoreboard displayed - Just found a minimal bug in one extreme case: When the ball touches the extreme corner of the paddle

--- srcs/docker/website/dist/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-window.addEventListener("load", () => {
-    const canvas = document.getElementById("pong_table");
-    const ctx = canvas.getContext("2d");
-    if (ctx) {
-        ctx.fillStyle = "pink";
-        ctx.fillRect(50, 50, 100, 100);
-    }
-});
--- srcs/docker/website/typescript/main.ts
+++ srcs/docker/website/typescript/main.ts
@@ -2,14 +2,20 @@ const canvas = document.getElementById("pong_table") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 const keysPressed: { [key: string]: boolean } = {};
 
+const leftScoreElem = document.getElementById("left-score")!;
+const rightScoreElem = document.getElementById("right-score")!;
+let leftPlayerScore = 0;
+let rightPlayerScore = 0;
+const maxScore = 5;
+
 document.addEventListener("keydown", (event) => {
 	keysPressed[event.key] = true;
 });
-  
+
 document.addEventListener("keyup", (event) => {
 	keysPressed[event.key] = false;
 });
-  
+
 const leftPaddle = {
 	x: 0,
 	y: 0,
@@ -24,16 +30,73 @@ const rightPaddle = {
 	height: 125
 };
 
+const ball = {
+	x: canvas.width / 2,
+	y: canvas.height / 2,
+	radius: 5,
+	speedX: 2,
+	speedY: 2
+};
+
+function collision_with_paddle()
+{
+	if (ball.x - ball.radius < leftPaddle.x + leftPaddle.width && ball.y > leftPaddle.y && ball.y < leftPaddle.y + leftPaddle.height)
+		ball.speedX = -ball.speedX;
+  
+  	if (ball.x + ball.radius > rightPaddle.x && ball.y > rightPaddle.y && ball.y < rightPaddle.y + rightPaddle.height)
+		ball.speedX = -ball.speedX;
+}
+
+let direction = 1;
+function resetBall()
+{
+	direction *= -1;
+	ball.x = canvas.width / 2;
+	ball.y = canvas.height / 2;
+	ball.speedX = direction * ball.speedX;
+}
+
+function count_score()
+{
+	ctx.fillStyle = 'white';
+	ctx.font = '30px Arial';
+	ctx.fillText(leftPlayerScore.toString(), canvas.width * 0.25, 50);
+	ctx.fillText(rightPlayerScore.toString(), canvas.width * 0.75, 50);
+}
+
+function count_and_reset_ball()
+{
+	if (ball.x + ball.radius < 0)
+	{
+		rightPlayerScore++;
+		rightScoreElem.textContent = rightPlayerScore.toString();
+		resetBall();
+	}
+	else if (ball.x - ball.radius > canvas.width)
+	{
+		leftPlayerScore++;
+		leftScoreElem.textContent = leftPlayerScore.toString();
+		resetBall();
+	}
+}
+
 function update()
 {
 	if ((keysPressed["w"] || keysPressed["W"]) && (leftPaddle.y > 0))
-	  leftPaddle.y -= 25; // Haut
+	  leftPaddle.y -= 25; // Up
 	if ((keysPressed["s"] || keysPressed["S"]) && (leftPaddle.y < canvas.height - leftPaddle.height))
-	  leftPaddle.y += 25; // Bas
+	  leftPaddle.y += 25; // Down
 	if ((keysPressed["ArrowUp"]) && (rightPaddle.y > 0))
 		rightPaddle.y -= 25;
 	if ((keysPressed["ArrowDown"]) && (rightPaddle.y < canvas.height - rightPaddle.height))
 		rightPaddle.y += 25;
+
+	ball.x += ball.speedX * 3;
+	ball.y += ball.speedY * 3;
+	if (ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height)
+		ball.speedY = -ball.speedY;
+	collision_with_paddle();
+	count_and_reset_ball();
 }
 
 function draw()
@@ -46,7 +109,15 @@ function draw()
 	ctx.fillStyle = 'white';
 	ctx.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
 	ctx.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
-  
+	
+	ctx.beginPath();
+	ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+	ctx.fill();
+
+	if (leftPlayerScore >= maxScore)
+		return(setTimeout(() => {alert("Left Player won!")}, 0));
+	if (rightPlayerScore >= maxScore)
+		return(setTimeout(() => {alert("Right Player won!")}, 0));
 	requestAnimationFrame(draw);
 }
 
