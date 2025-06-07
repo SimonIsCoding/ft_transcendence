d9f7dcf0de5ff36fa0f9f89a58a1a961cc75cc66
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-06-07 15:41:15 +0200

    left and right paddle smooth movement

--- srcs/docker/website/typescript/main.ts
+++ srcs/docker/website/typescript/main.ts
@@ -1,6 +1,15 @@
 const canvas = document.getElementById("pong_table") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
+const keysPressed: { [key: string]: boolean } = {};
 
+document.addEventListener("keydown", (event) => {
+	keysPressed[event.key] = true;
+});
+  
+document.addEventListener("keyup", (event) => {
+	keysPressed[event.key] = false;
+});
+  
 const leftPaddle = {
 	x: 0,
 	y: 0,
@@ -8,24 +17,35 @@ const leftPaddle = {
 	height: 125
 };
 
-document.addEventListener("keydown", handleKeyDown);
+const rightPaddle = {
+	x: canvas.width - 10,
+	y: 0,
+	width: 10,
+	height: 125
+};
 
-function handleKeyDown(event: KeyboardEvent)
+function update()
 {
-	if ((event.key === "w" || event.key === "W") && (leftPaddle.y > 0))
+	if ((keysPressed["w"] || keysPressed["W"]) && (leftPaddle.y > 0))
 	  leftPaddle.y -= 25; // Haut
-	else if ((event.key === "s" || event.key === "S") && (leftPaddle.y < canvas.height - leftPaddle.height))
+	if ((keysPressed["s"] || keysPressed["S"]) && (leftPaddle.y < canvas.height - leftPaddle.height))
 	  leftPaddle.y += 25; // Bas
+	if ((keysPressed["ArrowUp"]) && (rightPaddle.y > 0))
+		rightPaddle.y -= 25;
+	if ((keysPressed["ArrowDown"]) && (rightPaddle.y < canvas.height - rightPaddle.height))
+		rightPaddle.y += 25;
 }
 
 function draw()
 {
+	update();
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = 'black';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
   
 	ctx.fillStyle = 'white';
 	ctx.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
+	ctx.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
   
 	requestAnimationFrame(draw);
 }
