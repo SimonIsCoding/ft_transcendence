eef9b5d8d0a17df323e0c14b457e7ac608af48f1
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-06-12 13:33:19 +0200

    changing server.js into backend/.

--- srcs/docker/website/backend/server.js
+++ srcs/docker/website/backend/server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const pong_database = require('./db.js');
 
 app.use(express.static('public'));
 
 app.use('/website', express.static('public'));
 app.use('/dist', express.static('dist'));
 
-app.get('/players', (req, res) => {
-	res.json(player);
-});
-
 app.listen(PORT, () => {
-	console.log(`Server listening on http://localhost:${PORT}`);
+    console.log(`Server listening on http://localhost:${PORT}`);
 });
