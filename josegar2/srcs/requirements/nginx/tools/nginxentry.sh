312a3dc8bbd7989f05e7897e5a230976698c7e2b
josegar2 <josegar2@student.42barcelona.com>
2025-06-18 21:27:29 +0200

    not working version of nginx docker

---
+++ josegar2/srcs/requirements/nginx/tools/nginxentry.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# Copy/process secrets/configs (if needed)
+[ -f $FULLCHAIN_FILE ] && {
+  mkdir -p /etc/nginx/certs
+  cp $FULLCHAIN_FILE /etc/nginx/certs/fullchain.pem
+  cp $PRIVKEY_FILE /etc/nginx/certs/privkey.pem
+  chmod 400 /etc/nginx/certs/*
+  chown www-data:www-data /etc/nginx/certs/*
+}
+
+chown -R www-data:www-data /var/www/html
+
+mkdir -p /var/cache/nginx /var/log/nginx
+chown -R www-data:www-data /var/cache/nginx /var/log/nginx
+chown -R www-data:www-data /var/lib/nginx
+
+chmod 644 /etc/nginx/nginx.conf
+ls -l /etc/nginx/ngin*
+
+touch /run/nginx.pid
+chown www-data:www-data /run/nginx.pid
+chmod 644 /run/nginx.pid 
+
+# Launch NGINX as PID 1
+exec gosu www-data nginx -g "daemon off;"
+
