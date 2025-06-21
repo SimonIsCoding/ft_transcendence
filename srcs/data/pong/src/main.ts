6e0308165db88225cb719ffcb9888e3024e4579b
josegar2 <josegar2@student.42barcelona.com>
2025-06-21 18:44:16 +0200

    Modified template to use tailwindcss 'language' in html, css and ts

--- srcs/data/pong/src/main.ts
+++ srcs/data/pong/src/main.ts
@@ -4,21 +4,28 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="max-w-[1280px] mx-auto p-8 text-center font-sans text-[rgba(255,255,255,0.87)] dark:text-[rgba(255,255,255,0.87)] bg-white dark:bg-[#242424] min-h-screen flex flex-col items-center justify-center">
+    <div class="flex gap-4 justify-center items-center mb-6">
+      <a href="https://vite.dev" target="_blank" class="font-medium text-[#646cff] hover:text-[#535bf2] no-underline">
+        <img src="${viteLogo}" class="h-[6em] p-6 transition-[filter] duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank" class="font-medium text-[#646cff] hover:text-[#535bf2] no-underline">
+        <img src="${typescriptLogo}" class="h-[6em] p-6 transition-[filter] duration-300 hover:drop-shadow-[0_0_2em_#3178c6aa]" alt="TypeScript logo" />
+      </a>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <h1 class="text-[#213547] dark:text-white text-[3.2em] leading-[1.1] ">Vite + TailwindCSS + TypeScript</h1>
+    <div class="p-8">
+      <button
+        id="counter"
+        type="button"
+        class="bg-[#f9f9f9] text-black dark:bg-[#1a1a1a] dark:text-white border border-transparent px-[1.2em] py-[0.6em] text-[1em] font-medium rounded-[8px] cursor-pointer transition-colors duration-200 hover:border-[#646cff] focus:outline focus:outline-4"
+      >
+        count is 0
+      </button>
+    </div>
+    <p class="text-[#888] mt-4">Click on the Vite and TypeScript logos to learn more</p>
   </div>
-`
+`;
+
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
--- srcs/data/pong/tailwind.config.js
+++ srcs/data/pong/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{ts,js}"],
+  content: ["./*.html",
+	        "./src/**/*.{ts,js}"],
+  darkMode: "media",
   theme: {
     extend: {},
   },
