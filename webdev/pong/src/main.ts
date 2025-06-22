fefb2e3795c785a59635e67d5c8da5380cb339fe
josegar2 <josegar2@inception.myguest.virtualbox.org>
2025-06-22 17:43:07 +0200

    solved dark mode of button, and learning CSS

--- webdev/pong/src/main.ts
+++ webdev/pong/src/main.ts
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="max-w-[1280px] mx-auto p-8 text-center font-sans text-[rgba(255,255,255,0.87)] dark:text-[rgba(255,255,255,0.87)] bg-white dark:bg-[#242424] min-h-screen flex flex-col items-center justify-center">
+  <div class="max-w-[1280px] mx-auto p-8 text-center font-sans text-white/87 dark:text-white bg-white dark:bg-[#242424] min-h-screen flex flex-col items-center justify-center">
     <div class="flex gap-4 justify-center items-center mb-6">
       <a href="https://vite.dev" target="_blank" class="font-medium text-[#646cff] hover:text-[#535bf2] no-underline">
         <img src="${viteLogo}" class="h-[6em] p-6 transition-[filter] duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]" alt="Vite logo" />
@@ -18,7 +18,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <button
         id="counter"
         type="button"
-			class="bg-[#f9f9f9] text-black dark:bg-[#1a1a1a] dark:text-white border border-transparent px-[1.2em] py-[0.6em] text-[1em] font-medium rounded-[8px] cursor-pointer transition-colors duration-200 hover:border-[#646cff] focus:outline focus:outline-4"
+	class="bg-[#f9f9f9] ![color:black] dark:![color:white] dark:bg-[#1a1a1a] border border-transparent px-[1.2em] py-[0.6em] text-[1em] font-medium rounded-[8px] cursor-pointer transition-colors duration-200 hover:border-[#646cff] focus:outline focus:outline-4 !dark:text-white"
 		>
         count is 0
       </button>
