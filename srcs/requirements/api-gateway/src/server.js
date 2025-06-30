ef8b4bcb72ab0cc212e51fd52ace8be029c06c76
SimArch21 <simon.archambault@hotmail.fr>
2025-06-30 11:15:39 +0200

    vite compiling well with me now. thanks god I can go forward

--- srcs/requirements/api-gateway/src/server.js
+++ srcs/requirements/api-gateway/src/server.js
@@ -1,17 +1,40 @@
 import Fastify from 'fastify';
 import fetch from 'node-fetch';  // Explicit import
+import fastifyHttpProxy from '@fastify/http-proxy';
+import fs from 'fs';
+import fastifyCors from '@fastify/cors';
 
-const fastify = Fastify({ logger: true });
+const fastify = Fastify({
+  logger: true,
+  https: {
+    key: fs.readFileSync('/run/secrets/api_key'),
+    cert: fs.readFileSync('/run/secrets/api_cert')
+  }
+});
+
+// Register CORS plugin
+fastify.register(fastifyCors, {
+  origin: [
+    'https://localhost:4443',       // campus computer
+    'https://localhost'       // virtual server
+  ],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
+  // Optional: Cache preflight for 24h
+  maxAge: 86400
+});
 
 // Route to demonstrate gateway routing
 fastify.get('/api', async (request, reply) => {
   return { message: "API Gateway → Try /api/a or /api/b" };
 });
 
-// Proxy to Service A
-fastify.get('/api/a', async (request, reply) => {
-  const response = await fetch('http://service-a:3001/');
-  return response.json();
+// Proxy to Auth Service
+fastify.register(fastifyHttpProxy, {
+  upstream: 'http://auth-service:3001',
+  prefix: '/api/auth',
+  rewritePrefix: '/'
 });
 
 // Proxy to Service B
