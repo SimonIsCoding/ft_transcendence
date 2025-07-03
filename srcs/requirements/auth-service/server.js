734efcbbddcb716d0eacb03e00fbe478bfeaf042
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-07-03 17:49:15 +0200

    it is good but the console display that the credentials are always good. While I want them to be good when login and password are matching

---
+++ srcs/requirements/auth-service/server.js
@@ -0,0 +1,14 @@
+import fastify from 'fastify';
+import loginRoute from './src/login';
+
+const app = fastify();
+
+app.register(loginRoute); // tu enregistres bien la route
+
+app.listen({ port: 3001, host: '0.0.0.0' }, err => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log('Auth service running on http://localhost:3001');
+});
