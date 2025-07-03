49c924f00c919a444369c36ff202668e1b2438bd
josegar2 <josegar2@student.42barcelona.com>
2025-07-03 23:55:42 +0200

    some bugs

--- webdev/pong/src/views/home.ts
+++ webdev/pong/src/views/home.ts
@@ -25,7 +25,7 @@ export const HomeView = {
         </aside>
 
         <!-- Game Area -->
-        <main class="flex-1 bg-black p-4 p-0 overflow-hidden flex items-center justify-center">
+        <main class="flex-1 bg-black p-0 overflow-hidden flex items-center justify-center">
           ${GameView.renderGameCanvas()}
         </main>
 
