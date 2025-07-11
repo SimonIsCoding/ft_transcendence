f05da53866df4f2e34261b04907c7d266d123246
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-07-11 18:44:16 +0200

    designing login page

--- webdev/pong/src/views/login.ts
+++ webdev/pong/src/views/login.ts
@@ -3,15 +3,15 @@ import { initLoginAndRegistration } from '../services/loginAndRegistration';
 export class LoginView {
   public static render(): string {
     return `
-	<div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+	<div id="connexionAndRegistration" class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
 		<h1 id="title" class="text-3xl font-bold mb-6 text-blue-600">Hi</h1>
-		<input type="text" id="login" placeholder="Login" class="mb-2 px-4 py-2 border rounded w-64" />
-		<input type="password" id="password" placeholder="Password" class="mb-4 px-4 py-2 border rounded w-64"/>
-		<div class="mb-4 px-4 py-2 rounded w-64 flex justify-between">
-			<button id="login-btn" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Log in</button>
-			<button id="registerBtn" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Register</button>
+			<input id="login" type="text" placeholder="Login" class="mb-2 px-4 py-2 border rounded w-64" />
+			<input id="password" type="password" placeholder="Password" class="mb-4 px-4 py-2 border rounded w-64"/>
+			<div class="mb-4 px-4 py-2 rounded w-64 flex justify-between">
+				<button id="login-btn" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Log in</button>
+				<button id="registerBtn" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Register</button>
 		</div>
-		<div id="welcome-div" style="display: none;"></div>
+		<p id="welcome-div" style="display: none;"></p>
 	</div>`;
   }
 
