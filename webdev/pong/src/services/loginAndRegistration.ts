e421d383aa7b9a775b659797dbfae41aacbe91d6
SimArch21 <simon.archambault@hotmail.fr>
2025-07-22 18:28:22 +0200

    working on register page. Now Im stuck with password toggle

--- webdev/pong/src/services/loginAndRegistration.ts
+++ webdev/pong/src/services/loginAndRegistration.ts
@@ -1,11 +1,63 @@
 import { playBtnClicked } from '../controllers/menuController';
 
+export function isValidEmail(email: string): boolean
+{
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+// function setupPasswordToggle(inputId: string, toggleBtnId: string, iconId: string): void
+// {
+// 	const input = document.getElementById(inputId) as HTMLInputElement | null;
+// 	const toggleBtn = document.getElementById(toggleBtnId);
+// 	const icon = document.getElementById(iconId) as SVGElement | null;
+
+// 	if (!input || !toggleBtn ||!icon)
+// 	{
+// 		console.log("entered in return condition in setupPasswordToggle function")
+// 		return;
+// 	}
+
+// 	let isVisible = false;
+// 	toggleBtn.addEventListener("click", () => {
+// 		isVisible = !isVisible;
+// 		input.type = isVisible ? "text" : "password";
+
+// 		icon.innerHTML = isVisible
+// 			? `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+// 					d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7
+// 					a10.05 10.05 0 012.943-4.418M15 12a3 3 0 11-6 0 3 3 0 016 0zM3 3l18 18" />`
+// 			: `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+// 					d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+// 			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+// 					d="M2.458 12C3.732 7.943 7.523 5 12 5
+// 					c4.477 0 8.268 2.943 9.542 7
+// 					-1.274 4.057-5.065 7-9.542 7
+// 					-4.477 0-8.268-2.943-9.542-7z" />`;
+// 	});
+// }
+
 // --- form to log in
 export function initLogin()
 {
-	const submitBtn = document.getElementById("connectionBtn") as HTMLButtonElement;
+	// setupPasswordToggle("newPassword", "togglePassword", "eyeIcon");
+	// setupPasswordToggle("confirmPassword", "toggleConfirmPassword", "eyeIconConfirm");
 
+	const toggleBtn = document.getElementById("togglePassword");
+	const passwordInput = document.getElementById("newPassword") as HTMLInputElement;
+	console.log("toggleBtn:", toggleBtn);
+
+	console.log("🎯 Script chargé 1!");
+	toggleBtn?.addEventListener("click", () => {
+	console.log("received click on toggleBtn");
+	const isHidden = passwordInput.type === "password";
+	passwordInput.type = isHidden ? "text" : "password";
+	toggleBtn.textContent = isHidden ? "Hide" : "Show";
+	});
+	const submitBtn = document.getElementById("connectionBtn") as HTMLButtonElement;
 	submitBtn.addEventListener("click", () => {
+		console.log("🎯 Script chargé 2!");
+
 		const login = (document.getElementById("login") as HTMLInputElement).value;
 		const password = (document.getElementById("password") as HTMLInputElement).value;
 
@@ -27,7 +79,7 @@ export function initLogin()
 			{
 				connectionMsg = document.createElement("p");
 				connectionMsg.id = "connectionMsg";
-				connectionMsg.classList.add("font-seven", "text-white", "uppercase", "px-1", "py-1", "text-2xl");
+				connectionMsg.classList.add("font-seven", "text-white", "px-1", "py-1", "text-2xl");
 				const connectionBtn = document.getElementById("connectionBtn");
 				if (connectionBtn)
 					connectionBtn.insertAdjacentElement("afterend", connectionMsg);
@@ -43,7 +95,7 @@ export function initLogin()
 				{
 					playBtnLoginPage = document.createElement("button");
 					playBtnLoginPage.id = "playBtnLoginPage";
-					playBtnLoginPage.classList.add("font-seven", "text-white", "uppercase", "px-1", "py-1", "text-5xl", "border", "border-white", "rounded", "px-6", "py-3", "w-90");
+					playBtnLoginPage.classList.add("font-seven", "text-white", "px-1", "py-1", "text-5xl", "border", "border-white", "rounded", "px-6", "py-3", "w-90");
 					playBtnLoginPage.textContent = "Play";
 					const connectionBtn = document.getElementById("connectionBtn");
 					if (connectionBtn)
@@ -69,35 +121,48 @@ export function initLogin()
 export function initRegistration()
 {
 	document.getElementById("createAccountBtn")?.addEventListener("click", () => {
+		
 		const username = (document.getElementById("newUsername") as HTMLInputElement).value;
 		const password = (document.getElementById("newPassword") as HTMLInputElement).value;
-		const alias = (document.getElementById("newAlias") as HTMLInputElement).value;
+		const confirmPassword = (document.getElementById("confirmPassword") as HTMLInputElement).value;
+		const mail = (document.getElementById("newMail") as HTMLInputElement).value;
 
-		fetch('/api/auth/register', {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ login: username, password, alias })
-		})
-		.then(res => res.json())
-		.then(data => {
-		console.log("Account created:", data);
 		const registrationMsgId = "registrationMsg";
 		let registrationMsg = document.getElementById(registrationMsgId) as HTMLParagraphElement | null;
+		const createAccountBtn = document.getElementById("createAccountBtn");
 		if (!registrationMsg)
 		{
 			registrationMsg = document.createElement("p");
 			registrationMsg.id = "registrationMsg";
-			registrationMsg.classList.add("font-seven", "text-white", "uppercase", "px-1", "py-1", "text-2xl");
-			const createAccountBtn = document.getElementById("createAccountBtn");
+			registrationMsg.classList.add("font-seven", "text-white", "px-1", "py-1", "text-2xl");
 			if (createAccountBtn)
 				createAccountBtn.insertAdjacentElement("afterend", registrationMsg);
 		}
-		if (data.success === true)
-			registrationMsg.textContent = `Account created. Welcome ${username}`;
-		else
-			registrationMsg.textContent = `The username '${username}' already exists. Try another one.`;
+		if (confirmPassword !== password)
+			return registrationMsg!.textContent = `The passwords are not matching.`;
+		if (!username || !password || !confirmPassword || !mail)
+			return registrationMsg!.textContent = `All fields has to be filled to create an account.`;
+		if (isValidEmail(mail) === false)
+			return registrationMsg!.textContent = `The mail format is not correct.`;			
+			
+		fetch('/api/auth/register', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ login: username, password, mail })
 		})
-		.catch(err => console.error(err));
+		.then(async res => {
+		const data = await res.json();
+
+		if (!res.ok)
+			throw new Error(data.error);
+
+		console.log("Account created:", data);
+		registrationMsg.textContent = `Account created. Welcome ${username}`;
+		})
+		.catch(err => {
+			if (err)
+				registrationMsg.textContent = err.message;
+		});
 	});
 }
 
--- webdev/pong/src/views/menu.ts
+++ webdev/pong/src/views/menu.ts
@@ -1,9 +1,9 @@
 export const loginView = {
   render: (): string => `
 	<div id="loginCredentials" class="flex flex-col justify-center items-center w-full space-y-10">
-		<input id="login" type="text" placeholder="Login" class="font-seven text-white uppercase px-6 py-3 text-5xl border border-white rounded w-120" />
-		<input id="password" type="password" placeholder="Password" class="font-seven text-white uppercase px-6 py-3 text-5xl border border-white rounded w-120" />
-		<button id="connectionBtn" class="font-seven text-white uppercase px-6 py-3 text-5xl border border-white rounded w-120">Log in</button>
+		<input id="login" type="text" placeholder="Login" class="font-seven text-white px-6 py-3 text-5xl border border-white rounded w-120" />
+		<input id="password" type="password" placeholder="Password" class="font-seven text-white px-6 py-3 text-5xl border border-white rounded w-120" />
+		<button id="connectionBtn" class="font-seven text-white px-6 py-3 text-5xl border border-white rounded w-120">Log in</button>
 	</div>
   `
 };
@@ -11,11 +11,31 @@ export const loginView = {
 export const registerView = {
   render: (): string => `
 	<div id="registerForm" class="flex flex-col justify-center items-center w-full space-y-10">
-		<input id="newUsername" placeholder="Username" class="font-seven text-white uppercase px-6 py-3 text-5xl border border-white rounded w-120"/><br/>	
-		<input id="newPassword" placeholder="Password" type="password" class="font-seven text-white uppercase px-6 py-3 text-5xl border border-white rounded w-120"/><br/>
-		<input id="newAlias" placeholder="Alias" class="font-seven text-white uppercase px-6 py-3 text-5xl border border-white rounded w-120"/><br/>
-		<button id="createAccountBtn" class="font-seven text-white uppercase px-3 py-1.5 text-5xl border border-white rounded w-120">Create Account</button>
-		<button id="backToLogin" class="font-seven text-white uppercase px-1 py-1 text-2xl underline">Click here to be back to log in</button>
+		<div class="relative">
+			<input id="newUsername" placeholder="Username" class="font-seven text-white px-3 px-1.5 text-3xl border border-white rounded w-120"/><br/>
+		</div>
+		<div class="relative">
+			<input id="newPassword" placeholder="Password" type="password" class="font-seven text-white px-3 px-1.5 text-3xl border border-white rounded w-120"/>
+			<button type="button" id="togglePassword" class="absolute right-2 top-1/2 transform -translate-y-1/2 text-sm text-blue-500">Show</button>
+			<br/>
+		</div>
+		<div class="relative">
+			<input id="confirmPassword" placeholder="Confirm Password" type="password" class="font-seven text-white px-3 px-1.5 text-3xl border border-white rounded w-120"/>
+			<button id="toggleConfirmPassword" type="button" class="absolute right-2 top-1/2 transform -translate-y-1/2 text-sm text-blue-500">
+			<svg id="eyeIconConfirm" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white" class="w-6 h-6">
+			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+				d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+				d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+			</svg>
+			</button>
+			<br/>
+		</div>
+		<!-- <input id="newAlias" placeholder="Alias" class="font-seven text-white px-3 px-1.5 text-3xl border border-white rounded w-120"/><br/> -->
+		<input id="newMail" placeholder="mail" type="email" class="font-seven text-white px-3 px-1.5 text-3xl border border-white rounded w-120"/>
+		<br/>
+		<button id="createAccountBtn" class="font-seven text-white px-2 py-1 text-3xl border border-white rounded w-120">Create Account</button>
+		<button id="backToLogin" class="font-seven text-white px-2 py-1 text-2xl underline">Click here to be back to log in</button>
 	</div>
   `
 };
@@ -26,10 +46,10 @@ export const chooseTypeOfGameView = {
 	<div class="absolute left-1/2 top-0 h-full border-l-4 border-white opacity-50 	border-dashed -translate-x-1/2">
 	</div>
 	<div id="leftCanvaPart" class="w-1/2 flex flex-col justify-center items-center space-y-10 bg-black">
-		<button id="OneVsOneBtn" class="font-seven text-white uppercase px-6 py-3 text-5xl border border-white rounded">1 VS 1</button>
+		<button id="OneVsOneBtn" class="font-seven text-white px-6 py-3 text-5xl border border-white rounded">1 VS 1</button>
 	</div>
 	<div id="rightCanvaPart" class="w-1/2 flex items-center justify-center bg-black">
-		<button id="tournamentBtn" class="font-seven text-white uppercase px-6 py-3 text-5xl border border-white rounded">TOURNAMENT</button>
+		<button id="tournamentBtn" class="font-seven text-white px-6 py-3 text-5xl border border-white rounded">TOURNAMENT</button>
 	</div>
   `
 };
@@ -39,11 +59,11 @@ export const infoView = {
 	<div class="absolute left-1/2 top-0 h-full border-l-4 border-white opacity-50 	border-dashed -translate-x-1/2">
 	</div>
 	<div id="leftCanvaPart" class="w-1/2 flex flex-col justify-center items-center space-y-10 bg-black">
-		<button id="profileSettingsBtn" class="font-seven text-white uppercase px-6 py-3 text-5xl border border-white rounded">Profile Settings</button>
-		<button id="logoutBtn" class="font-seven text-white uppercase px-6 py-3 text-5xl border border-white rounded">LOG OUT</button>
+		<button id="profileSettingsBtn" class="font-seven text-white px-6 py-3 text-5xl border border-white rounded">Profile Settings</button>
+		<button id="logoutBtn" class="font-seven text-white px-6 py-3 text-5xl border border-white rounded">LOG OUT</button>
 	</div>
 	<div id="rightCanvaPart" class="w-1/2 flex items-center justify-center bg-black">
-		<button id="DashboardBtn" class="font-seven text-white uppercase px-6 py-3 text-5xl border border-white rounded">DASHBOARD</button>
+		<button id="DashboardBtn" class="font-seven text-white px-6 py-3 text-5xl border border-white rounded">DASHBOARD</button>
 	</div>
   `
 };
