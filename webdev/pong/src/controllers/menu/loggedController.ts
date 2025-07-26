a28436ad4f2af14d4cb309bc4e98c948f15a2c55
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-07-26 11:23:08 +0200

    smooth unfolded menu

--- webdev/pong/src/controllers/menu/loggedController.ts
+++ webdev/pong/src/controllers/menu/loggedController.ts
@@ -4,13 +4,12 @@ export const loggedController = {
 	const playSidebarBtn = document.getElementById('playSidebarBtn');
 	const playSubmenu = document.getElementById('playSubmenu');
 	const sidebar = document.getElementById('sidebar');
-	sidebar?.classList.toggle('w-[6%]');
-	sidebar?.classList.toggle('w-[20%]');
 
 	if (playSidebarBtn && playSubmenu && sidebar)
 	{
 		playSidebarBtn.addEventListener('click', () => {
-			playSubmenu.classList.toggle('hidden');
+			playSubmenu?.classList.toggle('max-h-0');
+  			playSubmenu?.classList.toggle('max-h-screen');//max-w-48
 			sidebar.classList.toggle('expanded');
 		});
 	}
