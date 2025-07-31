ee9d12c393338c7f54b8676ab67f821fed55debc
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-07-31 14:16:39 +0200

    cookies & JWT are now working well -> when disconnected, we can not see the profile picture (if yes, it might be a problem with the cache config, try in troubleshooting mode)

--- srcs/requirements/auth-service/routes/infoUserRoute.js
+++ srcs/requirements/auth-service/routes/infoUserRoute.js
@@ -1,8 +1,15 @@
-// check if this code is useful - to forget for the moment if you are reading this file
-export async function infoUserRoute(fastify)
-{
-	fastify.get('/api/auth/info', { preHandler: [fastify.auth] }, async (request, reply) => {
-		const user = request.user;
-		return { message: `Welcome ${user.login}`, userId: user.userId, login: user.login, mail: user.mail };
-	});
-}
+// // check if this code is useful - to forget for the moment if you are reading this file
+// export async function infoUserRoute(fastify)
+// {
+// 	fastify.get('/api/auth/info', { preHandler: [fastify.auth] }, async (request, reply) => {
+// 		const user = request.user;
+// 		return { message: `Welcome ${user.login}`, userId: user.userId, login: user.login, mail: user.mail };
+// 	});
+// }
+
+// import { auth } from '../plugins/auth.js';
+
+fastify.get('/info', { preHandler: [fastify.auth] }, async (request, reply) => {
+	const { id, login, mail, profile_picture, token } = request.user;
+	return { id, login, mail, profile_picture, token };
+});
