f706672b8c3232ce30387287d77bcbfa733863ff
SimArch21 <simon.archambault@hotmail.fr>
2025-08-04 13:50:01 +0200

    working on gameSettings front part buttons

--- webdev/pong/src/views/sidebar/gameSettingsRender.ts
+++ webdev/pong/src/views/sidebar/gameSettingsRender.ts
@@ -5,12 +5,12 @@ export function gameSettingsRender():string
 		<div id="settingsSubmenu" class="submenu h-screen w-full absolute top-0 bg-[#fbd11b] flex flex-col overflow-hidden max-h-0 transition-[max-height] duration-450 z-50 space-y-5">
 			<p id="submenuSettingsName" class="font-bold text-center pt-5">Game Settings</p>
 			<div class="flex items-center pt-5 w-full">
-				<label for="ballSpeedSlider" class="font-bold basis-[35%] whitespace-nowrap text-sm pl-2 text-center">
+				<label for="ballSpeedSlider" class="font-bold basis-[40%] whitespace-nowrap text-sm pl-2 text-center">
 					Ball Speed
 				</label>
 				<input id="ballSpeedSlider" type="range" min="1" max="10" value="5"
 					class="basis-[40%] h-1 rounded cursor-pointer" />
-				<span id="ballSpeedValue" class="font-bold basis-[25%] text-center font-mono">
+				<span id="ballSpeedValue" class="font-bold basis-[20%] text-center font-mono">
 					5
 				</span>
 			</div>
