4c77ced898d62e5808546afd22b6c4383dcc3260
SimArch21 <simon.archambault@hotmail.fr>
2025-08-04 11:34:48 +0200

    modifying loginRoute and docker-compose.yml

--- srcs/requirements/auth-service/routes/loginRoute.js
+++ srcs/requirements/auth-service/routes/loginRoute.js
@@ -19,14 +19,14 @@ export async function loginRoute(fastify)
 	if(user && match)
 	{
 		const token = jwt.sign({ id: user.id, login: user.login, mail: user.mail, profile_picture: user.profile_picture }, SECRET, { expiresIn: '24h' });// try to comment profile picture to know if we can receive it only thanks to app.get('/api/auth/info'
-		reply.setCookie('token', token, {
+		return reply.setCookie('token', token, {
 			httpOnly: true, //ALWAYS PUT TRUE FOR PROD
 			secure: true,
 			sameSite: 'strict',
 			maxAge: 24 * 60 * 60 * 1000,
 			path: '/', // important !
 		})
-		.send({ success: true, message: 'Login succeed', id: user.id, login: user.login, mail: user.mail, token: token });	
+		.send({ success: true, message: 'Login succeed', id: user.id, login: user.login, mail: user.mail, token: token });
 	}
 	return reply.status(401).send({ error: 'incorrect Id', success: false});
 	});
