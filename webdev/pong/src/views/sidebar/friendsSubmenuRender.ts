630e52bac26f8ffec7d7f7ad8dbc2b88ab40b007
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-08-05 14:18:25 +0200

    cardFollowRequest improved

--- webdev/pong/src/views/sidebar/friendsSubmenuRender.ts
+++ webdev/pong/src/views/sidebar/friendsSubmenuRender.ts
@@ -4,28 +4,48 @@ import { renderBackButton } from './sidebarUtils.ts'
 export function followRequestCard(): string
 {
 	return `
-	<div>
-		<p id="followRequest" class="pl-4 self-start font-semibold text-sm">Follow Request</p>
-		<div class="flex flex-col rounded-2xl max-w-md space-y-3 shadow-lg shadow-gray-600 p-4 bg-black">
-			<div class="flex items-center space-x-4">
-				<img src="chemin/vers/image.jpg" class="w-13 h-13 rounded-full object-cover border border-black bg-[#fbd11b] text-black flex items-center justify-center text-xl font-bold group-hover:bg-black group-hover:text-[#fbd11b] transition shadow-md" />
-				
-				<div class="flex flex-col">
-					<p class="font-bold text-sm text-[#fbd11b]">FriendUsername</p>
-					<p class="text-sm text-[#fbd11b]">email@exemple.com</p>
-				</div>
+	<div class="flex flex-col rounded-2xl max-w-md space-y-5 shadow-base shadow-gray-600 pr-3 pl-3 pt-2 pb-2 bg-black">
+		<div class="flex items-center space-x-2">
+			<img src="chemin/vers/image.jpg" class="w-10 h-10 rounded-full object-cover border border-black bg-[#fbd11b] text-black flex items-center justify-center text-xl font-bold group-hover:bg-black group-hover:text-[#fbd11b] transition shadow-md" />
+			
+			<div class="flex flex-col">
+				<p class="font-bold text-sm text-[#fbd11b]">FriendUsername</p>
+				<p class="text-sm text-[#fbd11b]">email@exemple.com</p>
 			</div>
+		</div>
 
-			<div class="flex justify-center space-x-3">
-				<button class="px-4 py-1 rounded-full border border-[#fbd11b] text-[#fbd11b] bg-black hover:bg-[#fbd11b] hover:text-black transition font-bold text-sm w-40">
-					Accept
-				</button>
-				<button class="px-4 py-1 rounded-full border border-[#fbd11b] text-[#fbd11b] bg-black hover:bg-[#fbd11b] hover:text-black transition font-bold text-sm w-40">
-					Ignore
-				</button>
+		<div class="flex justify-center space-x-2">
+			<button class="px-2 py-1 rounded-full border border-[#fbd11b] text-[#fbd11b] bg-black hover:bg-[#fbd11b] hover:text-black transition font-bold text-sm w-40">
+				Accept
+			</button>
+			<button class="px-2 py-1 rounded-full border border-[#fbd11b] text-[#fbd11b] bg-black hover:bg-[#fbd11b] hover:text-black transition font-bold text-sm w-30">
+				Ignore
+			</button>
+		</div>
+	</div>
+	`
+}
+
+export function friendsCard(): string
+{
+	return `
+	<div class="flex flex-col rounded-2xl max-w-md space-y-3 shadow-lg shadow-gray-600 p-4 bg-black">
+		<div class="flex items-center space-x-4">
+			<img src="chemin/vers/image.jpg" class="w-13 h-13 rounded-full object-cover border border-black bg-[#fbd11b] text-black flex items-center justify-center text-xl font-bold group-hover:bg-black group-hover:text-[#fbd11b] transition shadow-md" />
+			
+			<div class="flex flex-col">
+				<p class="font-bold text-sm text-[#fbd11b]">FriendUsername</p>
+				<p class="text-sm text-[#fbd11b]">email@exemple.com</p>
 			</div>
 		</div>
 	</div>
+	`
+}
+
+export function othersFriendsCard(): string
+{
+	return `
+	
 	`
 }
 
@@ -36,7 +56,16 @@ export function friendsSubmenuRender():string
 		${renderBackButton("backBtnFriendsSubmenu")}
 		<p id="submenuFriendsName" class="font-bold text-center pt-5">Friends List</p>
 		<hr class="w-full border-t-1.5 border-black" />
-		${followRequestCard()}
+		<div id="followRequestDiv">
+			<p id="followRequest" class="pl-4 self-start font-semibold text-sm">Follow Request</p>
+			${followRequestCard()}
+		</div>
+		<div id="friendsListDiv">
+			${friendsCard()}
+		</div>
+		<div id="othersFriendsDiv">
+			${othersFriendsCard()}
+		</div>
 	</div>
 	`
 }
