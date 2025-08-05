ce38e59ea6677a0d6f0a54bebf1bd92f76aadac1
SimArch21 <simon.archambault@hotmail.fr>
2025-08-05 13:12:16 +0200

    followRequestCard

--- webdev/pong/src/views/sidebar/friendsSubmenuRender.ts
+++ webdev/pong/src/views/sidebar/friendsSubmenuRender.ts
@@ -1,4 +1,33 @@
 import { renderBackButton } from './sidebarUtils.ts'
+// <button id="addFriendsBtn" class="border text-center font-semibold rounded-full text-base w-70 hover:bg-black hover:text-yellow-400 transition">+ Add Friends</button>
+
+export function followRequestCard(): string
+{
+	return `
+	<div>
+		<p id="followRequest" class="pl-4 self-start font-semibold text-sm">Follow Request</p>
+		<div class="flex flex-col rounded-2xl max-w-md space-y-3 shadow-lg shadow-gray-600 p-4 bg-black">
+			<div class="flex items-center space-x-4">
+				<img src="chemin/vers/image.jpg" class="w-13 h-13 rounded-full object-cover border border-black bg-[#fbd11b] text-black flex items-center justify-center text-xl font-bold group-hover:bg-black group-hover:text-[#fbd11b] transition shadow-md" />
+				
+				<div class="flex flex-col">
+					<p class="font-bold text-sm text-[#fbd11b]">FriendUsername</p>
+					<p class="text-sm text-[#fbd11b]">email@exemple.com</p>
+				</div>
+			</div>
+
+			<div class="flex justify-center space-x-3">
+				<button class="px-4 py-1 rounded-full border border-[#fbd11b] text-[#fbd11b] bg-black hover:bg-[#fbd11b] hover:text-black transition font-bold text-sm w-40">
+					Accept
+				</button>
+				<button class="px-4 py-1 rounded-full border border-[#fbd11b] text-[#fbd11b] bg-black hover:bg-[#fbd11b] hover:text-black transition font-bold text-sm w-40">
+					Ignore
+				</button>
+			</div>
+		</div>
+	</div>
+	`
+}
 
 export function friendsSubmenuRender():string 
 {
@@ -7,29 +36,7 @@ export function friendsSubmenuRender():string
 		${renderBackButton("backBtnFriendsSubmenu")}
 		<p id="submenuFriendsName" class="font-bold text-center pt-5">Friends List</p>
 		<hr class="w-full border-t-1.5 border-black" />
-		<button id="addFriendsBtn" class="border text-center font-semibold rounded-full text-base w-70 hover:bg-black hover:text-yellow-400 transition">+ Add Friends</button>
-		<div>
-			<p id="followRequest" class="pl-4 self-start font-semibold text-sm">Follow Request</p>
-			<div class="flex flex-col rounded-2xl max-w-md space-y-3 shadow-lg shadow-gray-600 p-4">
-				<div class="flex items-center space-x-4">
-					<img src="chemin/vers/image.jpg" class="w-13 h-13 rounded-full object-cover border border-black" />
-
-					<div class="flex flex-col">
-					<p class="font-bold text-sm text-black">FriendUsername</p>
-					<p class="text-sm text-gray-500">email@exemple.com</p>
-					</div>
-				</div>
-
-				<div class="flex justify-center space-x-3">
-					<button class="px-4 py-1 rounded-full border border-black text-black hover:bg-black hover:text-[#fbd11b] transition font-bold text-sm w-40">
-					Accept
-					</button>
-					<button class="px-4 py-1 rounded-full border border-black text-black hover:bg-black hover:text-[#fbd11b] transition font-bold text-sm w-40">
-					Ignore
-					</button>
-				</div>
-			</div>
-		</div>
+		${followRequestCard()}
 	</div>
 	`
 }
