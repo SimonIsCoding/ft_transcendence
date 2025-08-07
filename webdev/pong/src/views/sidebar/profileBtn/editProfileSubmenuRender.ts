c67977fae2d3a6d340fe3903a35b470d66135a11
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-08-07 16:14:49 +0200

    Edit profile submenu adding input

--- webdev/pong/src/views/sidebar/profileBtn/editProfileSubmenuRender.ts
+++ webdev/pong/src/views/sidebar/profileBtn/editProfileSubmenuRender.ts
@@ -10,6 +10,23 @@ export function editProfileSubmenuRender()
 		</button>
 		<p id="editProfileSubmenuName" class="font-bold text-center pt-5">Edit Profile</p>
 		<hr class="border-t-1.5 border-black w-full" />
+				<input type="file" id="uploadProfilePictureInput" accept="image/*" class="hidden">
+		<button id="uploadPictureBtn" class="relative w-24 h-24 bg-black rounded-full flex items-center justify-center border border-transparent hover:border-black group hover:bg-[#fbd11b] transition">
+			<svg id="uploadIcon" xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 text-[#fbd11b] group-hover:text-black transition" fill="currentColor" viewBox="0 0 24 24">
+				<path d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 5v3h20v-3c0-3.3-6.7-5-10-5z"/>
+			</svg>
+			<span class="absolute bottom-0 right-0 z-20 bg-[#fbd11b] text-black rounded-full w-5 h-5 flex items-center justify-center text-xl font-bold group-hover:bg-black group-hover:text-[#fbd11b] transition shadow-md">+</span>
+			<img id="previewProfilePicture" class="absolute w-24 h-24 rounded-full object-cover hidden" />
+		</button>
+
+		<p>Change password</p>
+		<input id="currentPasswordEditProfile" type="password" placeholder="Current password" class="border border-black w-[80%] rounded-xl text-black flex felx-col justify-center text-center placeholder-black/50 placeholder:text-sm h-[4%]">
+		<input id="changePasswordEditProfile" type="password" placeholder="Change password" class="border border-black w-[80%] rounded-xl text-black flex felx-col justify-center text-center placeholder-black/50 placeholder:text-sm h-[4%]">
+		<input id="repeatPasswordEditProfile" type="password" placeholder="Repeat password" class="border border-black w-[80%] rounded-xl text-black flex felx-col justify-center text-center placeholder-black/50 placeholder:text-sm h-[4%]">
+		<hr class="border-t-1.5 border-black w-20"/>
+		<p>Change mail</p>
+		<input id="changeMailEditProfile" type="text" placeholder="Change email" class="border border-black w-[80%] rounded-xl text-black flex felx-col justify-center text-center placeholder-black/50 placeholder:text-sm h-[4%]">
+		<button id="saveBtnEditProfile" class="text-gray-500 underline">Save</button>
 	</div>
 	`
 }
