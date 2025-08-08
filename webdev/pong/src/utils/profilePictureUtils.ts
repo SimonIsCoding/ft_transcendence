36ac9f1b1cdfbfabe308d0fe5dbf41cf9d239d87
SimArch21 <simon.archambault@hotmail.fr>
2025-08-08 16:50:27 +0200

    uploading picture is now available on editProfile. This part is literally done

--- webdev/pong/src/utils/profilePictureUtils.ts
+++ webdev/pong/src/utils/profilePictureUtils.ts
@@ -2,9 +2,10 @@ import { receiveProfilePicture } from "./utils";
 
 export async function uploadProfilePicture() : Promise<void>
 {
-	const uploadBtn = document.getElementById('uploadPictureBtn');
+	const uploadBtn = document.getElementById('uploadPictureBtnEditProfile');
 	const uploadInput = document.getElementById('uploadProfilePictureInput') as HTMLInputElement;
 	const preview = document.getElementById('previewProfilePicture') as HTMLImageElement;
+	const previewEdit = document.getElementById('previewProfilePictureEditProfile') as HTMLImageElement;
 	const uploadIcon = document.getElementById('uploadIcon')!;
 
 	return new Promise((resolve, reject) => {
@@ -27,6 +28,8 @@ export async function uploadProfilePicture() : Promise<void>
 			const result = e.target!.result as string;
 			preview.src = result;
 			preview.classList.remove('hidden');
+			previewEdit.src = result;
+			previewEdit.classList.remove('hidden');
 			uploadIcon.classList.add('hidden');
 		};
 		reader.readAsDataURL(file);
@@ -41,6 +44,7 @@ export async function uploadProfilePicture() : Promise<void>
 export async function loadExistingProfilePicture(): Promise<void>
 {
 	const preview = document.getElementById('previewProfilePicture') as HTMLImageElement;
+	const previewEdit = document.getElementById('previewProfilePictureEditProfile') as HTMLImageElement;
 	const uploadIcon = document.getElementById('uploadIcon')!;
 
 	try
@@ -53,16 +57,20 @@ export async function loadExistingProfilePicture(): Promise<void>
 			return; // not connected
 
 		const data = await res.json();
-		if (data && data.profile_picture)
+		if (data && data.profile_picture)//to load avatar pic
 		{
 			preview.src = `https://localhost:4443/${data.profile_picture}`;
 			preview.classList.remove('hidden');
+			previewEdit.src = `https://localhost:4443/${data.profile_picture}`;
+			previewEdit.classList.remove('hidden');
 			uploadIcon.classList.add('hidden');
 		}
-		else
+		else // to load personal pic
 		{
 			preview.src = data.profile_picture;
 			preview.classList.remove("hidden");
+			previewEdit.src = data.profile_picture;
+			previewEdit.classList.remove("hidden");
 		}
 	}
 	catch (err)
