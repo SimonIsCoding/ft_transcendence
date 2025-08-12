3bafbc41df181c391ab40a5938dcd0c5d9f1ec68
SimArch21 <simon.archambault@hotmail.fr>
2025-08-12 20:56:22 +0200

    working on follow request part - I nearly displaying the friend that sent the followRequests on the otherUser account

--- srcs/requirements/auth-service/routes/manageFriends.js
+++ srcs/requirements/auth-service/routes/manageFriends.js
@@ -33,3 +33,33 @@ export async function sendFriendRequestRoute(fastify)
 		return reply.status(200).send({success: true});
 	})
 }
+
+export async function getUserByIdRoute(fastify)
+{
+	fastify.post('/getUserById', async (request, reply) => {
+		const { userId } = request.body;
+		const stmt = db.prepare("SELECT id, login, mail, profile_picture FROM users WHERE id = ?");
+		const user = stmt.get(userId);
+		console.log("in getUserByIdRoute, user = ", user);
+		return user;
+	})
+}
+
+export async function requestFriendExistsRoute(fastify)
+{
+	console.log("in db for /requestFriendExists");
+	fastify.get('/requestFriendExists', async (request, reply) => {
+		// console.log("do we reached this line 1?");
+		const userFullInfo = request.user;
+		// console.log("do we reached this line 2?");
+		// console.log("userFullInfo = ", userFullInfo);
+		// const stmt = db.prepare('SELECT fr.*, u.* FROM friend_requests fr JOIN users u ON fr.to_user_id = u.id');//WHERE status = pending
+		// const stmt = db.prepare('SELECT fr.*, u.* FROM friend_requests fr JOIN users u ON fr.to_user_id = u.id');
+		const stmt = db.prepare("SELECT from_user_id, to_user_id FROM friend_requests WHERE status = 'pending'");
+    	const users = stmt.all();
+		// const user = stmt.run(userFullInfo);
+		// console	.log("stmt = " , stmt);
+		console.log("in db for /requestFriendExists, user =", users);
+		return reply.status(200).send(users);
+	});
+}
