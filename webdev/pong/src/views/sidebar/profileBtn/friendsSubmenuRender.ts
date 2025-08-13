7209e07b4d7725ca3d2d0c21f132010b32e41de3
SimArch21 <simon.archambault@hotmail.fr>
2025-08-13 19:04:16 +0200

    Friends List - adding scrool bar when the div is too horizontally big

--- webdev/pong/src/views/sidebar/profileBtn/friendsSubmenuRender.ts
+++ webdev/pong/src/views/sidebar/profileBtn/friendsSubmenuRender.ts
@@ -125,7 +125,7 @@ export const othersUsersCard = {
 export function friendsSubmenuRender():string 
 {
 	return `
-	<div id="friendsSubmenu" class="submenu h-screen w-full top-0 bg-[#fbd11b] flex items-center flex-col max-h-0 transition-[max-height] duration-450 z-50 space-y-5">
+	<div id="friendsSubmenu" class="submenu h-screen w-full top-0 bg-[#fbd11b] flex items-center flex-col max-h-0 transition-[max-height] duration-450 z-50 space-y-5 overflow-y-auto max-h-screen">
 		${renderBackButton("backBtnFriendsSubmenu")}
 		<p id="submenuFriendsName" class="font-bold text-center pt-5">Friends List</p>
 		<hr class="w-full border-t-1.5 border-black" />
