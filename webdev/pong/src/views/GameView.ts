718e5730bbd13bfc048195628456729bf4a8556c
josegar2 <josegar2@student.42barcelona.com>
2025-09-07 11:20:56 +0200

    1st try of 1vs1 route

---
+++ webdev/pong/src/views/GameView.ts
@@ -0,0 +1,65 @@
+// import type { User } from "../config";
+import { handleSidebar } from "./sidebar/sidebarBehavior";
+import { oneVsOneArea } from "./OneVsOneArea";
+import { oneVsAIArea } from "./OneVsOneArea";
+import { GameRender } from "../pong-erik/GameRender";
+import { ShowGame } from "../pong-erik/ShowGame";
+
+export const GameView = {
+  
+ render(): string {
+	return `
+	<div class="w-screen h-screen flex bg-[#fbd11b] overflow-hidden">
+		
+		<div id="popup" class="fixed top-4 right-4 bg-green-600 text-white px-4 py-3 rounded shadow-lg hidden z-50">
+		</div>
+
+		<div id="sidebar" class="bg-[#fbd11b] h-screen flex flex-col overflow-hidden transition-all duration-500 ease-in-out w-[64px]">
+		</div>
+	  ${oneVsOneArea.render()}
+	  ${oneVsAIArea.render()}
+      ${new GameRender().renderGame()}
+    </div>
+  `;
+  },
+
+  async init(): Promise<void>
+  {
+
+    await handleSidebar();
+	const gameArea = document.getElementById("gameArea");
+	const oneVsOneArea = document.getElementById("oneVsOneArea");
+	const oneVsAIArea = document.getElementById("oneVsAIArea");
+	
+	const player1 = document.getElementById("player1") as HTMLInputElement;
+    const player2 = document.getElementById("player2") as HTMLInputElement;
+    const player1VSAI = document.getElementById("player1VSAI") as HTMLInputElement;
+    let tmp = player2;
+
+	gameArea?.classList.add('hidden');
+	if (ShowGame.gameType === 'p-vs-p') {
+		oneVsOneArea?.classList.add('hidden');
+		oneVsAIArea?.classList.remove('hidden');
+	} else if (ShowGame.gameType === 'p-vs-ai') {
+		tmp = player1VSAI;
+		ShowGame.otherPlayer = tmp.value;
+		oneVsOneArea?.classList.add('hidden');
+		oneVsAIArea?.classList.remove('hidden');
+	}
+	if (!tmp && !player1) {
+	  new ShowGame().initGame({
+	  player1: { alias: "User 1" },
+		player2: { alias: "User 2" },
+		winner: null
+	  });
+	} else {
+	  new ShowGame().initGame({
+	  player1: { alias: player1.value },
+		player2: { alias: tmp.value },
+		winner: null
+	  });
+	}
+	
+
+  }
+};
