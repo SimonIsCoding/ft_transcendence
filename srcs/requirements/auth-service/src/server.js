31541d330aaa80a7939dbfb665c5f6303a4db157
SimArch21 <simon.archambault@hotmail.fr>
2025-09-07 10:02:46 +0200

    Username put by default on 1vsAI

--- srcs/requirements/auth-service/src/server.js
+++ srcs/requirements/auth-service/src/server.js
@@ -22,8 +22,6 @@ import { infoUserRoute } from '../routes/infoUserRoute.js';
 import {deleteExpiredSessions} from '../utils/sessionTokens.js';
 import { googleRoute } from '../routes/google.js';
 import { matchesRoutes } from '../routes/matchesRoute.js';
-import { tournamentsRoutes } from '../routes/tournamentsRoute.js';
-import { statsRoutes } from '../routes/statsRoute.js';
 
 // Load environment variables
 dotenv.config();
@@ -74,9 +72,7 @@ app.register(eraseAccountRoute); // delete /me
 app.register(FriendsRoute); // /friends routes
 app.register(logoutRoute);  // delete /me/sessions
 app.register(googleRoute);
-app.register(statsRoutes);
 app.register(matchesRoutes);
-app.register(tournamentsRoutes);
 
 // --- Cleanup expired sessions daily ---
 setInterval(() => {
--- webdev/pong/src/views/OneVsOneArea.ts
+++ webdev/pong/src/views/OneVsOneArea.ts
@@ -78,7 +78,7 @@ export const oneVsAIArea = {
   init(): void
   {
 	oneVsAIAreaInit();
+
+
   }
 }
-
-
